cb53655859a04019732eb6f215e1a5622af30ea3
Soxibjon <sohajon2005@gmail.com>
2025-02-19 10:43:01 +0500

    .

--- admin_app/admin.py
+++ admin_app/admin.py
@@ -3,13 +3,12 @@ from users_app.models import UserProgram, UserSubscription
 
 class UserProgramAdmin(admin.ModelAdmin):
     list_display = ["id", "user", "program", "start_date", "end_date", "get_is_paid"]
-    list_filter = ["get_is_paid"]
 
     def get_is_paid(self, obj):
         subscription = UserSubscription.objects.filter(user=obj.user, is_active=True).first()
         return subscription.is_active if subscription else False
 
     get_is_paid.short_description = "Is Paid"
-    get_is_paid.boolean = True  # Show as a boolean (✔/❌) in the admin panel
+    get_is_paid.boolean = True  # Shows as a boolean (✔/❌) in the admin panel
 
 admin.site.register(UserProgram, UserProgramAdmin)
