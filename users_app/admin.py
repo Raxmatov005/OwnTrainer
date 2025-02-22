0b93f2b6a251c5ea97fefbed80de7e9a9dc4200d
Soxibjon <sohajon2005@gmail.com>
2025-02-22 21:01:22 +0500

    .

--- users_app/admin.py
+++ users_app/admin.py
@@ -1,18 +1,16 @@
 from django.contrib import admin
 
 from users_app.models import (User, UserProgram, UserProgress, Program, Preparation, PreparationSteps,
-                              SessionCompletion, ExerciseCompletion, MealCompletion, Exercise, WorkoutCategory,
+                              SessionCompletion, MealCompletion, Exercise,
                               Notification, Meal, Session)
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(UserProgress)
 admin.site.register(Program)
-admin.site.register(WorkoutCategory)
 admin.site.register(Session)
 admin.site.register(SessionCompletion)
 admin.site.register(Exercise)
-admin.site.register(ExerciseCompletion)
 admin.site.register(Meal)
 admin.site.register(MealCompletion)
 admin.site.register(Preparation)
