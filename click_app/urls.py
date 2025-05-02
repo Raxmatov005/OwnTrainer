645aec43050f33d9a629d50856e7729cb6cb79b5
Soxibjon <sohajon2005@gmail.com>
2025-05-02 14:57:06 +0500

    .

--- click_app/urls.py
+++ click_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.CreateClickOrderView.as_view()),
     path('click/transaction/', views.OrderTestView.as_view()),
-    path('payment/click/prepare/', ClickPrepareAPIView.as_view(), name='click-prepare'),
-    path('payment/click/complete/', ClickCompleteAPIView.as_view(), name='click-complete'),
+    path('payment/click/prepare/', views.ClickPrepareAPIView.as_view(), name='click-prepare'),
+    path('payment/click/complete/', views.ClickCompleteAPIView.as_view(), name='click-complete'),
 ]
