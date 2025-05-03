662e26e7c983709075fc3922866b6425ff26cc94
Soxibjon <sohajon2005@gmail.com>
2025-05-03 21:14:47 +0500

    .

--- click_app/views.py
+++ click_app/views.py
@@ -125,7 +125,7 @@ class ClickPrepareAPIView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        logger.info(f"Click Prepare request at {timezone.now()}: {request.data}, Headers: {request.headers}")
+        logger.info(f"Click Prepare request at {timezone.now()}: Data={request.data}, Headers={request.headers}, Method={request.method}, IP={request.META.get('REMOTE_ADDR')}")
         try:
             order_id = request.data.get("order_id")
             amount = int(request.data.get("amount"))
@@ -163,7 +163,7 @@ class ClickCompleteAPIView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        logger.info(f"Click Complete request at {timezone.now()}: {request.data}, Headers: {request.headers}")
+        logger.info(f"Click Complete request at {timezone.now()}: Data={request.data}, Headers={request.headers}, Method={request.method}, IP={request.META.get('REMOTE_ADDR')}")
         try:
             order_id = request.data.get("order_id")
             amount = int(request.data.get("amount"))
